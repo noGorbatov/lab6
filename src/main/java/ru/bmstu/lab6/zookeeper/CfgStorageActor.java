092ab5473842518d58f1c1ecca1456293ea2efc0
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:12:38 +0300

    Scripted auto-commit on change (2021-12-25 15:12:38) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/CfgStorageActor.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/CfgStorageActor.java
@@ -1,11 +1,21 @@
 package ru.bmstu.lab6.zookeeper;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
 
 public class CfgStorageActor implements AbstractActor {
 
+    public static class StoreMsg {
+        private final ArrayList<Integer> ports;
+        public StoreMsg
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        ReceiveBuilder.create()
+                .match()
+                .build();
     }
 }
