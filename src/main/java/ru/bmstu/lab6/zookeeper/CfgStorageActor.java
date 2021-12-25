6b01288dde2f9cd55c6405fa825a916e12b1fa42
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:12:26 +0300

    Scripted auto-commit on change (2021-12-25 15:12:26) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/CfgStorageActor.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/CfgStorageActor.java
@@ -1,2 +1,11 @@
-package ru.bmstu.lab6.zookeeper;public class CfgStorageActor {
+package ru.bmstu.lab6.zookeeper;
+
+import akka.actor.AbstractActor;
+
+public class CfgStorageActor implements AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
