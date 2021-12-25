71d8ecb04a28088ad0f7c25375276da38cd3fec6
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:24:41 +0300

    Scripted auto-commit on change (2021-12-25 15:24:41) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/ZookeeperClient.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/ZookeeperClient.java
@@ -1,6 +1,9 @@
 package ru.bmstu.lab6.zookeeper;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
@@ -10,17 +13,22 @@ public class ZookeeperClient {
     private static final int SESSION_TIMEOUT_MS = 5000;
     private static final String BASE_NODE_PATH = "/servers";
 
-    private String host;
-    private int port;
-    private ZooKeeper client;
-    public ZookeeperClient(String host, Integer port, ActorRef storageActor) throws IOException {
+    private final String host;
+    private final int port;
+    private final ZooKeeper client;
+    private final ActorRef storageActor;
+
+    public ZookeeperClient(String host, Integer port, ActorRef storageActor) throws IOException, InterruptedException, KeeperException {
         this.host = host;
         this.port = port;
+        this.storageActor = storageActor;
         client = new ZooKeeper(SERVER_ADDR, SESSION_TIMEOUT_MS, event -> {
             System.out.println("event fired " + event);
         });
         client.create(BASE_NODE_PATH + "/s",
-                      port.toString().getBytes(),
-                      )
+                      (host + port).getBytes(),
+                      ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                      CreateMode.EPHEMERAL_SEQUENTIAL);
     }
+    private void update()
 }
