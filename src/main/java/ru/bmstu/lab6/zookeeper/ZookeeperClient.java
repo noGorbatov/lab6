7e83422f6dfc6739bf4eab38b88c983772fb2b3b
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:23:50 +0300

    Scripted auto-commit on change (2021-12-25 15:23:50) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/ZookeeperClient.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/ZookeeperClient.java
@@ -3,13 +3,14 @@ package ru.bmstu.lab6.zookeeper;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperClient {
+    private static final String SERVER_ADDR = "localhost:2181";
     private String host;
     private int port;
     private ZooKeeper client;
     public ZookeeperClient(String host, int port) {
         this.host = host;
         this.port = port;
-        client = 
+        client =
 
 
     }
