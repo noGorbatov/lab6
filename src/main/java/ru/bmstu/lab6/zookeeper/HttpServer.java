b3e563a68dc2f738fe8157680d290e4bc8a5ef13
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:38:53 +0300

    Scripted auto-commit on change (2021-12-25 15:38:53) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
@@ -59,7 +59,7 @@ public class HttpServer extends AllDirectives {
                             CfgStorageActor.ResRandomServerMsg res =
                                     (CfgStorageActor.ResRandomServerMsg) resp;
                             String server = res.getServer();
-                            System.out.println("request for " + server);
+                            System.out.println("request for " + server + " got count " + count);
                             String reqUrl = createUrl(server, url, count - 1);
                             return fetch(reqUrl);
                         }));
