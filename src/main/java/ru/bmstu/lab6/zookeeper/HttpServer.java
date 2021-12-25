dcf3135eb9877477dd6832172c0d789dfb68928c
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:19:21 +0300

    Scripted auto-commit on change (2021-12-25 15:19:21) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
@@ -4,19 +4,28 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.pattern.PatternsCS;
+
+import java.util.concurrent.CompletionStage;
 
 public class HttpServer extends AllDirectives {
     private static final String URL_PARAM = "url";
     private static final String COUNT_PARAM = "count";
+    private static final int ACTOR_TIMEOUT_MS = 4000;
 
     private final Http http;
     private final String host;
     private final int port;
     private final ActorRef storageActor;
 
-    private  
+    private CompletionStage<HttpResponse> fetch(String url) {
+        return http.singleRequest(HttpRequest.create(url));
+    }
 
     public Route createRoute() {
         return concat(
@@ -24,8 +33,14 @@ public class HttpServer extends AllDirectives {
                     parameter(COUNT_PARAM, countString -> {
                         int count = Integer.parseInt(countString);
                         if (count == 0) {
-
+                            return completeWithFuture(fetch(url));
                         }
+
+                        return completeWithFuture(PatternsCS.ask(storageActor,
+                                    new CfgStorageActor.GetRandomServerMsg(),
+                                    ACTOR_TIMEOUT_MS).thenCompose( resp -> {
+                                        
+                        })
                     })
                 }))
         )
