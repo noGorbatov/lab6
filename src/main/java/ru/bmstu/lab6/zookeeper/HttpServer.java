b74d2431ccac4498b9c0826da6e084da31b6c7dc
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 15:18:30 +0300

    Scripted auto-commit on change (2021-12-25 15:18:30) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/zookeeper/HttpServer.java
@@ -4,12 +4,16 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
 public class HttpServer extends AllDirectives {
-    private static final  
+    private static final String URL_PARAM = "url";
+    private static final String COUNT_PARAM = "count";
     public Route createRoute() {
         return concat(
-                get( () -> parameter())
-        )
-    }
+                get( () -> parameter(URL_PARAM, url -> {
+                    parameter(COUNT_PARAM, count -> {
+
+                })
+                    )
+    })
 
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
